057728beacea2efab153d01bddd9319747dbd0ad
zhiweiliu <zhiweiliu@salesforce.com>
2024-05-20 17:08:38 -0700

    updating example

--- example/SearchActions.py
+++ example/SearchActions.py
@@ -1,7 +1,7 @@
 import os
 
-from langchain_community.tools import DuckDuckGoSearchResults, WikipediaQueryRun
-from langchain_community.utilities import WikipediaAPIWrapper
+import wikipedia
+import duckduckgo_search
 
 from agentlite.actions.BaseAction import BaseAction
 
@@ -11,25 +11,28 @@ class DuckSearch(BaseAction):
         action_name = "DuckDuckGo_Search"
         action_desc = "Using this action to search online content."
         params_doc = {"query": "the search string. be simple."}
-        self.search = DuckDuckGoSearchResults()
+        self.ddgs = duckduckgo_search.DDGS()
         super().__init__(
             action_name=action_name, action_desc=action_desc, params_doc=params_doc,
         )
 
     def __call__(self, query):
-        return self.search.run(query)
-
-
+        results = self.ddgs.chat(query)
+        return results
+    
 class WikipediaSearch(BaseAction):
     def __init__(self) -> None:
         action_name = "Wikipedia_Search"
         action_desc = "Using this API to search Wiki content."
         params_doc = {"query": "the search string. be simple."}
 
-        self.search = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
         super().__init__(
             action_name=action_name, action_desc=action_desc, params_doc=params_doc,
         )
 
     def __call__(self, query):
-        return self.search.run(query)
+        search_results = wikipedia.search(query)
+        if not search_results:
+            return "No results found."
+        article = wikipedia.page(search_results[0])
+        return article.summary
