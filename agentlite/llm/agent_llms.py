4c5ff181cf501cc13e6aa011ae472c86062f4d93
zhiweiliu <zhiweiliu@salesforce.com>
2024-06-06 21:17:35 -0700

    deleting ollama provider as it requires additional dependencies

--- agentlite/llm/agent_llms.py
+++ agentlite/llm/agent_llms.py
@@ -91,37 +91,34 @@ class LangchainChatModel(BaseLLM):
         return self.llm_chain.run(prompt)
 
 
-class LangchainOllamaLLM(BaseLLM):
-    def __init__(self, llm_config: LLMConfig):
-        from langchain_community.llms import Ollama
+# class LangchainOllamaLLM(BaseLLM):
+#     def __init__(self, llm_config: LLMConfig):
+#         from langchain_community.llms import Ollama
 
-        super().__init__(llm_config)
-        llm = Ollama(
-            model=self.llm_name,
-            temperature=self.temperature,
-            num_predict=self.max_tokens,
-            base_url=llm_config.base_url
-            # api_key=llm_config.api_key,
-        )
-        human_template = "{prompt}"
-        prompt = PromptTemplate(template=human_template, input_variables=["prompt"])
-        self.llm_chain = LLMChain(prompt=prompt, llm=llm)
+#         super().__init__(llm_config)
+#         llm = Ollama(
+#             model=self.llm_name,
+#             temperature=self.temperature,
+#             num_predict=self.max_tokens,
+#             base_url=llm_config.base_url
+#             # api_key=llm_config.api_key,
+#         )
+#         human_template = "{prompt}"
+#         prompt = PromptTemplate(template=human_template, input_variables=["prompt"])
+#         self.llm_chain = LLMChain(prompt=prompt, llm=llm)
 
-    def run(self, prompt: str):
-        return self.llm_chain.run(prompt)
-
-
+#     def run(self, prompt: str):
+#         return self.llm_chain.run(prompt)
 
 def get_llm_backend(llm_config: LLMConfig):
     llm_name = llm_config.llm_name
     llm_provider = llm_config.provider
-
     if llm_name in OPENAI_CHAT_MODELS:
         return LangchainChatModel(llm_config)
     elif llm_name in OPENAI_LLM_MODELS:
         return LangchainLLM(llm_config)
     else:
-        if llm_provider == "ollama":
-            return LangchainOllamaLLM(llm_config)
-        else:
-            return LangchainLLM(llm_config)
+        return LangchainLLM(llm_config)
+    # TODO: add more llm providers and inference APIs but for now we are using langchainLLM as the default
+    # Using other LLM providers will require additional setup and configuration
+    # We suggest subclass BaseLLM and implement the run method for the specific provider in your own best practices
